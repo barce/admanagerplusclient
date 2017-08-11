8d8de311a3963975826caf4e8259cffc58748b6f
Barce <james.barcelona@accuenmedia.com>
2017-08-11 13:57:28 -0700

    version bump; fixed init.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='admanagerplusclient',
-    version='0.0.5',
+    version='0.0.6',
     author='Jim Barcelona',
     author_email='barce@me.com',
     packages=['admanagerplusclient', 'admanagerplusclient.tests'],
