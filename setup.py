4f91c238933e2749aa7bff01a0702f1ed16fd35e
Barce <james.barcelona@accuenmedia.com>
2017-09-22 18:06:40 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='admanagerplusclient',
-    version='0.0.12',
+    version='0.1.0',
     author='Jim Barcelona',
     author_email='barce@me.com',
     packages=['admanagerplusclient', 'admanagerplusclient.tests'],
@@ -13,6 +13,6 @@ setup(
     url='http://pypi.python.org/pypi/admanagerplusclient/',
     license='LICENSE',
     description='A client for interacting with the Ad Manager Plus Platform.',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
 )
  
