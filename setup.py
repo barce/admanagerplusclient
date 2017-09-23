b4312cb90d2b2cbba79998d993b8ece368373577
Barce <james.barcelona@accuenmedia.com>
2017-09-22 17:59:14 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='admanagerplusclient',
-    version='0.0.9',
+    version='0.0.10',
     author='Jim Barcelona',
     author_email='barce@me.com',
     packages=['admanagerplusclient', 'admanagerplusclient.tests'],
