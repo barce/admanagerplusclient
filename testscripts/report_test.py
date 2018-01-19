488f63c9055a7c697f90bad74dbbad145c72f82c
Barce <james.barcelona@accuenmedia.com>
2018-01-19 10:45:36 -0800

    remove extraneous tests for report_test.py

--- testscripts/report_test.py
+++ testscripts/report_test.py
@@ -11,61 +11,6 @@ brc = ampclient.BrightRollClient()
 
 brc.refresh_access_token()
 
-# test for edits
-
-campaign = brc.traffic_type_by_id('campaigns', 86184)
-print(campaign)
-
-print('change status')
-print('OLD status:')
-print(campaign['response']['status'])
-print('NEW status:')
-print('PAUSED')
-print('running update with complete object...')
-resp = brc.update_traffic_type('campaigns', 86184, json.dumps(campaign['response']))
-
-exit()
-
-print('fetching updated campaign...')
-campaign = brc.traffic_type_by_id('campaigns', 86184)
-print(campaign)
-
-print('running update with just one column changed...')
-resp = brc.update_traffic_type('campaigns', 86184, json.dumps({"status": "PAUSED"}))
-print('fetching updated campaign...')
-campaign = brc.traffic_type_by_id('campaigns', 86184)
-print(campaign)
-
-# advertisers = brc.traffic_type_by_id('advertisers', 20500)
-# print('--- advertisers ---')
-# print(advertisers)
-# 
-# print('--- campaign by id ---')
-# campaigns = brc.traffic_type_by_id('campaigns', 44582)
-# print(campaigns)
-# 
-# print('--- campaign by query string id ---')
-# campaigns = brc.traffic_types('campaigns?accountId=20500')
-# print(campaigns)
-
-# print('--- campaign traffic types by filter ---')
-# campaigns = brc.traffic_types_by_filter('campaigns', 20500)
-# print(campaigns)
-# 
-# print('--- line by filter ---')
-# lines = brc.traffic_types_by_filter('lines', 44582)
-# print(lines)
-# print('--- lines by query string orderId ---')
-# lines = brc.traffic_types('lines?orderId=44582')
-# print(lines)
-
-
-# print('--- deals by query string id ---')
-# deals = brc.traffic_types('deals?accountId=20500')
-# print(deals)
-
-# EXIT
-exit()
 
 # -d '{"reportOption": {"timezone": "America/New_York", "dimensionTypeIds": [1, 3, 4, 5, 6, 8, 9, 34, 50, 63], "metricTypeIds": [1, 2, 23, 25, 29, 44, 46, 100, 101, 116, 117, 129, 166, 167], "currency": 4}, "intervalTypeId": 1, "startDate": "2017-09-18T00:00:00-05:00", "endDate": "2017-09-18T11:59:59-05:00", "dateTypeId": 11}'
 
