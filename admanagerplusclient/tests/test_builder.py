49a2ff783b4fbd1fb099fe9a8cd05e38694e4a81
jonjharrison <jj2harrison@gmail.com>
2019-07-25 11:33:52 -0700

    Repo cleanup

--- admanagerplusclient/tests/test_builder.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-from admanagerplusclient import ampclient
-
-class TestBrightRollClient(TestCase):
-    def test_config(self):
-        b = ampclient.BrightRollClient()
-        self.assertTrue(isinstance(b, ampclient.BrightRollClient))
-
