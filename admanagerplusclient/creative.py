94432d609c758c03cd97c2fa9e922627488bb939
jonjharrison <jj2harrison@gmail.com>
2019-07-30 14:07:07 -0700

    Get methods for creatives

--- admanagerplusclient/creative.py
+++ admanagerplusclient/creative.py
@@ -3,14 +3,14 @@ from admanagerplusclient.base import Base
 
 class Creative(Base):
     def get_creatives_by_lineitem(self, lineitem_id, seat_id):
-        url = f"{self.dsp_host}/traffic/lines"
-        url += f"/?orderId={campaign_id}&seatId={str(seat_id)}"
+        url = f"{self.dsp_host}/traffic/ads"
+        url += f"/?lineId={lineitem_id}&seatId={str(seat_id)}"
 
         r = self.make_request(url, self.headers, 'GET')
         return r
 
-    def get_one(self, line_id, seat_id):
-        url = f"{self.dsp_host}/traffic/lines/{str(line_id)}"
+    def get_one(self, creative_id, seat_id):
+        url = f"{self.dsp_host}/traffic/ads/{str(creative_id)}"
         url += "/?seatId=" + str(seat_id)
 
         r = self.make_request(url, self.headers, 'GET')
--- tests/test_creative.py
+++ tests/test_creative.py
@@ -14,15 +14,15 @@ class CreativeTestCase(unittest.TestCase):
     def test_get_creatives_by_lineitem(self):
         c = Creative(CreativeTestCase.connection)
 
-        creatives = json.loads(c.get_creatives_by_lineitem(97893, 92))
-        print(lines)
+        creatives = json.loads(c.get_creatives_by_lineitem(376875, 92))
+        print(creatives)
 
-        self.assertEqual(lines["response_code"], 200)
+        self.assertEqual(creatives["response_code"], 200)
 
     def test_get_one_creative(self):
         c = Creative(CreativeTestCase.connection)
 
-        creative = json.loads(c.get_one(376875, 92))
+        creative = json.loads(c.get_one(1977315, 92))
         print(creative)
 
-        self.assertEqual(line["response_code"], 200)
+        self.assertEqual(creative["response_code"], 200)
