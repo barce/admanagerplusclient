19aafbd72868e3e323746e724d82abc77eb54ca2
jonjharrison <jj2harrison@gmail.com>
2019-08-06 20:17:28 -0700

    Set inventory directly in line item

--- admanagerplusclient/lineitem.py
+++ admanagerplusclient/lineitem.py
@@ -113,6 +113,61 @@ class LineItem(Base):
             }
         )
 
+    def create_sitelist(self, advertiser_id, seat_id, name, list_type, list):
+            app_domain_data = []
+            for item in list:
+                app_domain_data.append({"itemName": str(item)})
+
+            payload = {
+                "accountId": advertiser_id,
+                "name": name,
+                "status": "ACTIVE",
+                "type": list_type,
+                "items": app_domain_data
+            }
+
+            url = self.dsp_host + "/traffic/sitelists/?seatId={0}".format(seat_id)
+            r = self.make_request(url, self.headers, 'POST', data=payload)
+            return r
+
+    def update_sitelist(self, id, advertiser_id, seat_id, name, list_type, list):
+        app_domain_data = []
+        for item in list:
+            app_domain_data.append({"itemName": str(item)})
+
+        payload = {
+            "accountId": advertiser_id,
+            "name": name,
+            "status": "ACTIVE",
+            "type": list_type,
+            "items": app_domain_data
+        }
+
+        url = self.dsp_host + "/traffic/sitelists/" + str(id) + "?seatId={0}".format(seat_id)
+        r = self.make_request(url, self.headers, 'PUT', data=payload)
+        return r
+
+    def set_sitelists(self, add_site_list_ids, remove_site_list_ids=[]):
+        site_list_data = []
+        for id in add_site_list_ids:
+            rval = {
+                "excluded": False,
+                "entityId": int(id)
+            }
+            site_list_data.append(rval)
+
+        self.inventory_payload["siteLists"] = {
+            "removed": remove_site_list_ids,
+            "clearAll": False,
+            "added": site_list_data
+        }
+        self.inventory_payload["types"].append(
+            {
+                "name": "SITE_LISTS",
+                "isTargeted": True
+            }
+        )
+
     def update_inventory(self, seat_id):
         url = self.dsp_host + "/traffic/lines/{0}/targeting?seatId={1}".format(int(self.dsp_lineitem_id), seat_id)
         r = self.make_request(url, self.headers, 'POST', self.inventory_payload)
